2e027f0cb6934d61eaa524da710c9c00ed4f7684
jalilbengoufa <jalilbengoufa@gmail.com>
2018-11-07 11:49:38 -0500

    working v

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
     "github.com/gorilla/mux"
+    "path/filepath"
+    "flag"
+    "strconv"
 )
 var servers []Server
 type Server struct {
@@ -30,49 +33,50 @@ func exitsAndStatusAvailble( addr string) bool{
     }
     return false
 }
-func BootServer(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r);
-	if (exitsAndStatusAvailble(params["MacAddress"])){
-		resp := struct {
-			K string   `json:"kernel"`
-			I []string `json:"initrd"`
-			CMD string `json:"cmdline"`
-		}{
-			K: "http://tinycorelinux.net/7.x/x86/release/distribution_files/vmlinuz64",
-			I: []string{
-				"http://tinycorelinux.net/7.x/x86/release/distribution_files/rootfs.gz",
-				"http://tinycorelinux.net/7.x/x86/release/distribution_files/modules64.gz",
-			},
-			CMD: "coreos.autologin coreos.first_boot=1 coreos.config.url={{ ID \"https://files.local/cloud-config\" }}",
-		}
+func BootServers(w http.ResponseWriter, r *http.Request) {
+	//params := mux.Vars(r);
+	//if (exitsAndStatusAvailble(params["MacAddress"])){
+	log.Printf("Serving boot config for %s", filepath.Base(r.URL.Path))
+	resp := struct {
+		K string   `json:"kernel"`
+		I []string `json:"initrd"`
+		CMD string `json:"cmdline"`
+	}{
+		K: "file:///home/cedille/coreos_production_pxe.vmlinuz",
+		I: []string{
+			"file:///home/cedille/coreos_production_pxe_image.cpio.gz",
+		},
+		CMD: "coreos.autologin coreos.first_boot=1 coreos.config.url={{ URL \"file:///home/cedille/pxe-config.ign\" }}",
 
-		if err := json.NewEncoder(w).Encode(&resp); err != nil {
-			panic(err)
-		}else{
-
-		}
 	}
+
+	json.NewEncoder(w).Encode(&resp); 
+	//}
 }
 
 func GetServers(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(servers)
 }
-func InstallServer(w http.ResponseWriter, r *http.Request) {
-	
-}
+func InstallServer(w http.ResponseWriter, r *http.Request) {}
 func InstallAll(w http.ResponseWriter, r *http.Request) {}
+func ResetServer(w http.ResponseWriter, r *http.Request) {}
 
+var (
+	port = flag.Int("port", 3000, "Port to listen on")
+)
 
 func main() {
+    flag.Parse()
     router := mux.NewRouter()
     //add servers availble
     servers = append(servers, Server{ID: "1", MacAddress: "dr-ee-6y-8o-ee", IPAddress: "168.104.0.10", DoneBoot:false})
 
     //routes
-    router.HandleFunc("/v1/boot/{MacAddress}", BootServer).Methods("GET")
     router.HandleFunc("/v1/install/{MacAddress}", InstallServer).Methods("GET")
     router.HandleFunc("/v1/install/all", InstallAll).Methods("GET")
-    router.HandleFunc("/v1/servers", GetServers).Methods("GET")
+    router.HandleFunc("/v1/reset/{MacAddress}", ResetServer).Methods("GET")
+    router.HandleFunc("/v1/info/", GetServers).Methods("GET")
+    router.HandleFunc("/v1/boot/{MacAddress}", BootServers).Methods("GET")
 
-    log.Fatal(http.ListenAndServe(":3000", router))
+    log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), router))
 }
