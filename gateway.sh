055842697cd2453a9f71ddf97a19dfd8e653831a
jalilbengoufa <jalilbengoufa@gmail.com>
2018-12-28 01:30:10 -0500

    creation des vm de test avec vagrant

--- gateway.sh
+++ gateway.sh
@@ -5,16 +5,9 @@ echo 'Defaults env_keep += "DEBIAN_FRONTEND"' >/etc/sudoers.d/env_keep_apt
 chmod 440 /etc/sudoers.d/env_keep_apt
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get upgrade -y
+#apt-get upgrade -y
 
 
-#
-# install tcpdump for being able to capture network traffic.
-
-apt-get install -y tcpdump
-
-
-#
 # install vim.
 
 apt-get install -y --no-install-recommends vim
@@ -27,7 +20,27 @@ set laststatus=2
 set nobackup
 EOF
 
+# install golang 
 
+wget https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
+tar -xvf go1.10.3.linux-amd64.tar.gz
+sudo mv go /usr/local
+export PATH=$PATH:/usr/local/go/bin
+
+# install git
+
+sudo apt install git -y
+
+# install pixiecore
+go get -v go.universe.tf/netboot/cmd/pixiecore
+
+# run pixiecore client 
+
+#sudo ~/go/bin/pixiecore api http://localhost:3000 --dhcp-no-bind
+
+# run pixiecore api
+
+cp /vagrant/pixicoreAPI ~/
 
 # provision the DHCP server.
 # see http://www.syslinux.org/wiki/index.php?title=PXELINUX
@@ -39,9 +52,9 @@ default-lease-time 300;
 max-lease-time 300;
 option domain-name-servers 8.8.8.8, 8.8.4.4;
 option subnet-mask 255.255.255.0;
-option routers 10.10.10.2;
-subnet 10.10.10.0 netmask 255.255.255.0 {
-  range 10.10.10.100 10.10.10.254;
+option routers 10.1.1.1;
+subnet 10.1.1.0 netmask 255.255.255.0 {
+  range 10.1.1.100 10.1.1.254;
 }
 
 EOF
@@ -70,7 +83,7 @@ sysctl net.ipv4.ip_forward=1
 sed -i -E 's,^\s*#?\s*(net.ipv4.ip_forward=).+,\11,g' /etc/sysctl.conf
 
 # NAT through eth0.
-# NB use something like -s 10.10.10/24 to limit to a specific network.
+# NB use something like -s 10.1.1/24 to limit to a specific network.
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # load iptables rules on boot.
@@ -80,3 +93,5 @@ cat<<'EOF'>/etc/network/if-pre-up.d/iptables-restore
 iptables-restore </etc/iptables-rules-v4.conf
 EOF
 chmod +x /etc/network/if-pre-up.d/iptables-restore
+
+
